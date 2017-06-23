06a5bbd4302b4f4c4ec70b9a670a331e693363a7
james lally <james.lally@drfoster.com>
2017-06-24 08:27:43 +1000

    first commit

---
+++ Excel-EPPlus/Program.cs
@@ -0,0 +1,23 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+
+namespace Excel_EPPlus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Sheet1");
+
+            for (int x = 1; x < 100; x++)
+                for (int y = 1; y < 10; y++)
+                {
+                    workSheet.Cells[x, y].Value = Guid.NewGuid().ToString();
+                }
+
+            excel.SaveAs(new FileInfo("newWorkbook.xlsx"));
+        }
+    }
+}
